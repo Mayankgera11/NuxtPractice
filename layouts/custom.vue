535c6893fc83dbb80b438b6c2baadddf67be931f
mayankgera0 <mayankgera79@gmail.com>
2024-07-12 16:09:14 +0530

    hello

---
+++ layouts/custom.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>custom layout</p>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ pages/custm.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: "custom",
+});
+</script>
+<template>
+  <div>custom</div>
+</template>
